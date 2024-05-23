453811112cf62aa54583799e6149a13ad571d295
Nikita Shevchenko <nikita.shevchenko@nordclan.com>
2024-05-23 10:28:35 +0300

    56. Merge Intervals

---
+++ LeetCode/56. Merge Intervals.js
@@ -0,0 +1,23 @@
+// Runtime 88ms Beats 82.77% of users with JavaScript
+// Memory 57.72 MB Beats 84.71% of users with JavaScript
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    const sIntervals = intervals.sort((a, b) => a[0] - b[0]);
+    const res = [intervals[0]];
+
+    for(let i = 1; i < intervals.length; i++) {
+        if (res.at(-1)[1] >= intervals[i][0]) {
+            if (res.at(-1)[1] < intervals[i][1]) {
+                res.at(-1)[1] = intervals[i][1];
+            }
+        } else {
+            res.push(intervals[i])
+        }
+    }
+
+    return res
+};
